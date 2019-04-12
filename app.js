2f7924669d05582ca7bdb39982909d0fddc6cf13
marcosvillabasa <marcosvillabasa@gmail.com>
2019-04-12 19:14:28 -0300

    agregada la ruta para los reportes

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 
 
 //inicializar variables
 const app = express();
+
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    extname: 'hbs'
+}));
+app.set('view engine', '.hbs');
 
 
 //body parser
@@ -27,12 +36,14 @@ mongoose.connection.openUri('mongodb://localhost:27017/InvestDB', (err, res) => 
 var appRoutes = require('./rutas/app');
 var usuarioRoutes = require('./rutas/usuario');
 var loginRoutes = require('./rutas/login');
+var reporteRoutes = require('./rutas/reporte');
 
 
 //rutas
 app.use('/', appRoutes);
 app.use('/usuarios', usuarioRoutes);
 app.use('/login', loginRoutes);
+app.use('/reporte', reporteRoutes);
 
 //Escuchar peticiones
 app.listen(3000, () => {
--- rutas/app.js
+++ rutas/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
 app.get('/', (req, res, next) => {
+
+
     res.status(200).json({
         ok: true,
         mensaje: 'Peticion Get realizada correctamente'
-    })
+    });
+
 });
 
 
